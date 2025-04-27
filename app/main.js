2e426e2507e9e870df42539b245bc0dc967a2a84
OrichalcosShunnoros <c3146092091@gmail.com>
2025-04-27 16:27:47 -0500

    first time instansed the electron app.

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow = null
+// When the application is ready, show an window
+app.on('ready', () => {
+  // This way prevent the elimination of the app when all windows are closed for the garbage collector
+  mainWindow = new BrowserWindow()
+})
