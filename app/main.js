7f36754b829468fb67520ea196694f5e94954b76
OrichalcosShunnoros <c3146092091@gmail.com>
2025-04-27 16:56:52 -0500

    first visualization created

--- app/main.js
+++ app/main.js
@@ -1,8 +1,22 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-let mainWindow = null
+let mainWindow = null;
 // When the application is ready, show an window
 app.on('ready', () => {
   // This way prevent the elimination of the app when all windows are closed for the garbage collector
-  mainWindow = new BrowserWindow()
+  mainWindow = new BrowserWindow({
+    // webPreferences is used to enable Node.js integration and disable context isolation and more options
+    webPreferences: {
+      nodeIntegration: true, // Enable Node.js integration
+      contextIsolation: false // Disable context isolation (recommended if using Node.js integration)
+    }
+  })
+
+  // This line is used to load the index.html file
+  mainWindow.loadFile(__dirname +'/index.html')
+
+  // When the application is closed, the window will be closed too
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  })
 })
