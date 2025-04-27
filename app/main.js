1821a9db5f90e92ec2d89251294bb3489440124e
OrichalcosShunnoros <c3146092091@gmail.com>
2025-04-27 17:16:03 -0500

    add function to capture text that write on textarea

--- app/main.js
+++ app/main.js
@@ -5,18 +5,24 @@ let mainWindow = null;
 app.on('ready', () => {
   // This way prevent the elimination of the app when all windows are closed for the garbage collector
   mainWindow = new BrowserWindow({
+    show: false, // the window don't show until it's ready
     // webPreferences is used to enable Node.js integration and disable context isolation and more options
     webPreferences: {
       nodeIntegration: true, // Enable Node.js integration
       contextIsolation: false // Disable context isolation (recommended if using Node.js integration)
     }
-  })
+  });
 
   // This line is used to load the index.html file
-  mainWindow.loadFile(__dirname +'/index.html')
+  mainWindow.loadFile(__dirname +'/index.html');
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show(); // Show the window when the files are loaded
+    mainWindow.webContents.openDevTools(); // Open the dev tools at the start of the app
+  });
 
   // When the application is closed, the window will be closed too
   mainWindow.on('closed', () => {
     mainWindow = null;
-  })
-})
+  });
+});
--- app/renderer.js
+++ app/renderer.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+
+// This function is used to convert the markdown to html
+const renderToMarkdown = markdown => {
+  htmlView.innerHTML = markdown; // This line sets the innerHTML of the htmlView to the markdown content
+}
+
+// This function captures the keyup event and calls the renderToMarkdown function
+markdownView.addEventListener('keyup', e => {
+  const currentContent = e.target.value; // This line gets the current content of the textarea
+  renderToMarkdown(currentContent); // This line calls the renderToMarkdown function and passes the current content of the textarea
+});
