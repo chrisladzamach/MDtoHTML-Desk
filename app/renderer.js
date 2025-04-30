2a8893281cda606a65336b07181519d9dc17b318
OrichalcosShunnoros <c3146092091@gmail.com>
2025-04-29 23:18:03 -0500

    finish practice

--- app/renderer.js
+++ app/renderer.js
@@ -1,9 +1,12 @@
+const marked = requite('marked')
+
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
 
 // This function is used to convert the markdown to html
 const renderToMarkdown = markdown => {
-  htmlView.innerHTML = markdown; // This line sets the innerHTML of the htmlView to the markdown content
+  // This line sets the innerHTML of the htmlView to the markdown content
+  htmlView.innerHTML = marked(markdown, { sanitize: true }); // The sanitize option is deprecated in marked v3.0.0, but it's used for verify the content of the markdown.
 }
 
 // This function captures the keyup event and calls the renderToMarkdown function
